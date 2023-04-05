dd9356c9842a6476d06806d7dc3b3c4e1b522841
dream7180 <dreamawake@163.com>
2023-04-05 11:12:58 +0800

    7.8-4

--- foobox/js_panels/jsplaylist/WSHsettings.js
+++ foobox/js_panels/jsplaylist/WSHsettings.js
@@ -1651,10 +1651,10 @@ oPage = function(id, objectName, label, nbrows) {
 			var rh = cSettings.rowHeight;
 			this.elements.push(new oRadioButton(0, 20, cSettings.topBarHeight + rh * 2.25, "System", (sys_scrollbar == true), "settings_radioboxes_action", this.id));
 			this.elements.push(new oRadioButton(1, zoom(120, zdpi), cSettings.topBarHeight + rh * 2.25, "Narrow", (sys_scrollbar == false), "settings_radioboxes_action", this.id));
-			this.elements.push(new oCheckBox(2, 20, cSettings.topBarHeight + rh * 4.25, "Record in file tag also", "rating2tag ? true : false", "settings_checkboxes_action", this.id));
-			this.elements.push(new oCheckBox(3, 20, cSettings.topBarHeight + rh * 6.25, "Cover/info panel follow cursor, not follow playback", "follow_cursor ? true : false", "settings_checkboxes_action", this.id));
-			this.elements.push(new oCheckBox(4, 20, cSettings.topBarHeight + rh * 7.25, "Highlight color follow cover", "color_bycover ? true : false", "settings_checkboxes_action", this.id));
-			this.elements.push(new oTextBox(5, txtbox_x, Math.ceil(cSettings.topBarHeight + rh * 8.75), oTextBox_3, cHeaderBar.height, "Cover filenames for groups by folder, put ';' between filenames", dir_cover_name, "settings_textboxes_action", this.id));			
+			this.elements.push(new oCheckBox(2, 20, cSettings.topBarHeight + rh * 4.25, "Overwrite file tag filed", "rating2tag ? true : false", "settings_checkboxes_action", this.id));
+			this.elements.push(new oCheckBox(3, 20, cSettings.topBarHeight + rh * 6.25, "Cover/info panel follows cursor, not follows playback", "follow_cursor ? true : false", "settings_checkboxes_action", this.id));
+			this.elements.push(new oCheckBox(4, 20, cSettings.topBarHeight + rh * 7.25, "Highlight color follows cover", "color_bycover ? true : false", "settings_checkboxes_action", this.id));
+			this.elements.push(new oTextBox(5, txtbox_x, Math.ceil(cSettings.topBarHeight + rh * 8.75), oTextBox_3, cHeaderBar.height, "Cover filenames for groups by directory, put ';' between filenames", dir_cover_name, "settings_textboxes_action", this.id));			
 			break;
 		};
 	};
